5bb69108cd5b8edab7a721ba7524908d85263d57
KimDongHyub <50415338+KimDongHyub@users.noreply.github.com>
2021-09-27 20:06:27 +0900

    Update and rename clock_div to clock_div.v

--- clock_div.v
+++ clock_div.v
@@ -3,4 +3,5 @@ reg [26:0]counter=0;
 always @(posedge Clk_100M) begin
 counter <= (counter>=9999)?0:counter+1; 
 slow_clk <= (counter < 5000)?1'b0:1'b1;
-end //입력받은 클럭으로 더 느린 클럭을 만듬 endmodule
+end //입력받은 클럭으로 더 느린 클럭을 만듦 
+endmodule
